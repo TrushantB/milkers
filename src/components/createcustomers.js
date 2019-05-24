e7686b2155155fd86d88ad105a17ae5e9999bbde
Trushant <trushant.ivision@gmail.com>
2019-05-24 18:28:37 +0530

    updata csv file  function

--- src/components/createcustomers.js
+++ src/components/createcustomers.js
@@ -2,6 +2,8 @@ import React,{Component} from 'react'
 import { Form, Input, Button, Upload, Icon,   Select, Checkbox,  Row, Col } from 'antd';
 import axios from 'axios';
 import Workbook from 'react-excel-workbook';
+import ReactFileReader from 'react-file-reader';
+
 class CreateCustomersForms extends React.Component {
     constructor() {
         super();
@@ -24,15 +26,6 @@ class CreateCustomersForms extends React.Component {
       componentDidMount() {
         axios.get('http://localhost:3005/DemoSheet').then((response) => {
             this.setState({dataSource:response.data})
-          })
-          axios.get('http://localhost:3005/BlankSheet').then((response) => {
-            this.setState({blankDataSource:response.data})
-          })
-      }
-      
-    render(){
-        const { getFieldDecorator } = this.props.form;
-        if(this.state.dataSource) {
             this.state.dataSource.map((item,index)=>{
                 for(let entry in item) {
                     this.state.sheets.push(
@@ -45,26 +38,28 @@ class CreateCustomersForms extends React.Component {
                    </Workbook.Sheet>
                   )
                 }
-                
             })
-         }
-
-             if(this.state.blankDataSource) {
-                this.state.blankDataSource.map((item,index)=>{
-                    for(let entry in item) {
-                        this.state.blankSheets.push(
-                        <Workbook.Sheet data={item[entry]} name={entry} key = {index}>
-                           <Workbook.Column label="name" />
-                           <Workbook.Column label="buff" />
-                           <Workbook.Column label="cow" />
-                           <Workbook.Column label="date" />
-                       </Workbook.Sheet>
-                      )
-                    }
-                    
-                })
-             }
-
+          })
+          
+          axios.get('http://localhost:3005/BlankSheet').then((response) => {
+            this.setState({blankDataSource:response.data})
+            this.state.blankDataSource.map((item,index)=>{
+                for(let entry in item) {
+                    this.state.blankSheets.push(
+                    <Workbook.Sheet data={item[entry]} name={entry} key = {index}>
+                       <Workbook.Column label="name" value={row => row.id}/>
+                       <Workbook.Column label="buff" value={row => row.id}/>
+                       <Workbook.Column label="cow" value={row => row.id}/>
+                       <Workbook.Column label="date" value={row => row.id}/>
+                   </Workbook.Sheet>
+                  )
+                }
+            })
+          })
+      }
+      
+    render(){
+        const { getFieldDecorator } = this.props.form;
         return(
             <div className="formbox">
                 <h2> Step 4:  Create Customers </h2>
@@ -91,7 +86,7 @@ class CreateCustomersForms extends React.Component {
                             
                         </Col>
                     </Row>
-<br />
+                   < br />
                     <Row>
                         <Col span={24} >
                             <h3> Upload Customer Data Sheet</h3>    
@@ -104,7 +99,18 @@ class CreateCustomersForms extends React.Component {
                                     <Button>
                                         <Icon type="upload" /> Click to upload
                                     </Button>
-                                    </Upload>,
+                                    </Upload>
+                                    // <ReactFileReader handleFiles={this.handleFiles} fileTypes={'.csv'} >
+                                            
+                                    //         <div style={{height:"700" , width:"400", backgroundColor:'orange'}}>
+
+                                    //         <center><Icon type="inbox"  />UPLOAD</center> 
+                                    //         </div>
+                                    // </ReactFileReader>
+
+
+
+
                                 )}
                             </Form.Item>                            
                         </Col>
